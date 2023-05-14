5f042d50b731f75643554b01710d0dfe2aba3665
Ravenusa <80948124+ravenusa@users.noreply.github.com>
2023-05-14 21:04:57 +0700

    update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const loginText = document.querySelector(".title-text .login");
       const loginForm = document.querySelector("form.login");
       const loginBtn = document.querySelector("label.login");
       const signupBtn = document.querySelector("label.signup");
-      const signupLink = document.querySelector("form .signup-link a");
+      // const signupLink = document.querySelector("form .signup-link a");
       signupBtn.onclick = (()=>{
         loginForm.style.marginLeft = "-50%";
         loginText.style.marginLeft = "-50%";
@@ -14,4 +14,4 @@ const loginText = document.querySelector(".title-text .login");
       signupLink.onclick = (()=>{
         signupBtn.click();
         return false;
-      });
+      }); 
